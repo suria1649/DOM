9c3b4a2b6ec1cdff58bb42a0fd382517bf1935df
suria1649 <suria1649@gmail.com>
2023-05-15 19:02:08 +0530

    update dom

--- index.js
+++ index.js
@@ -1,67 +1,67 @@
-// let doc = document.getElementById("root");
-// doc.innerHTML = '<h1>Welcome To Guvi</h1>'    
-// doc.innerText = '<h1>Welcome to Guvi<h1>'
-// doc.setAttribute('class','text-color');
-// doc.style.textAlign =  "center";
-// doc.style.fontFamily = "verdana";
-// doc.style.color = "red";
+let doc = document.getElementById("root");
+doc.innerHTML = '<h1>Welcome To Guvi</h1>'    
+doc.innerText = '<h1>Welcome to Guvi<h1>'
+doc.setAttribute('class','text-color');
+doc.style.textAlign =  "center";
+doc.style.fontFamily = "verdana";
+doc.style.color = "red";
 
-// let docs = document.getElementsByTagName("div");
-// console.log(docs);
+let docs = document.getElementsByTagName("div");
+console.log(docs);
 
-// for(let i=0 ; i<docs.length ; i++){
-//   docs[i].innerHTML = '<h1>Welcome To Guvi</h1>';
-//   docs[i].style.textAlign =  "center";
-//   docs[i].style.fontFamily = "verdana";
-//   docs[i].style.color = "red";
-// }
+for(let i=0 ; i<docs.length ; i++){
+  docs[i].innerHTML = '<h1>Welcome To Guvi</h1>';
+  docs[i].style.textAlign =  "center";
+  docs[i].style.fontFamily = "verdana";
+  docs[i].style.color = "red";
+}
 
-// let h1 = document.createElement("h1");
-// h1.innerHTML = "Iam learning javascript";
-// document.body.appendChild(h1);
+let h1 = document.createElement("h1");
+h1.innerHTML = "Iam learning javascript";
+document.body.appendChild(h1);
 
-// let h2 = document.createElement("h2");
-// h2.innerHTML = "we are late";
-// document.body.appendChild(h2);
+let h2 = document.createElement("h2");
+h2.innerHTML = "we are late";
+document.body.appendChild(h2);
 
-// h2.append("we should be responsible");
+h2.append("we should be responsible");
 
-// let parent = document.createElement("div");
-// document.body.appendChild(parent);
-// parent.style.display='flex';
-// parent.style.flexWrap='wrap';
+let parent = document.createElement("div");
+document.body.appendChild(parent);
+parent.style.display='flex';
+parent.style.flexWrap='wrap';
 
-// let child = document.createElement("div");
-// parent.appendChild(child);
+let child = document.createElement("div");
+parent.appendChild(child);
 
-// let image = document.createElement('img');
-// child.appendChild(image);
-
-
-// image.setAttribute('src','flag.jpg');
-// image.style.width='300px';
-// image.style.marginRight = '10px'
-
-// let child1 = document.createElement("div");
-// parent.appendChild(child1);
-
-// let image1 = document.createElement('img');
-// child1.appendChild(image1);
-
-// image1.setAttribute('src','flag.jpg');
-// image1.style.width='300px';
+let image = document.createElement('img');
+child.appendChild(image);
 
 
+image.setAttribute('src','flag.jpg');
+image.style.width='300px';
+image.style.marginRight = '10px'
+
+let child1 = document.createElement("div");
+parent.appendChild(child1);
+
+let image1 = document.createElement('img');
+child1.appendChild(image1);
+
+image1.setAttribute('src','flag.jpg');
+image1.style.width='300px';
 
 
 
-// let child2 = document.createElement("div");
-// document.body.appendChild(child2);
 
-// let image2 = document.createElement('img');
-// child1.appendChild(image2);
 
-// image2.setAttribute('src','flag.jpg');
+let child2 = document.createElement("div");
+document.body.appendChild(child2);
+
+let image2 = document.createElement('img');
+child1.appendChild(image2);
+
+image2.setAttribute('src','flag.jpg');
 
  
 
